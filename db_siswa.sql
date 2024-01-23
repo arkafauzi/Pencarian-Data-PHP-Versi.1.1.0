56a65abbef0cd83816e77a1dbf5d5b2f3b723efb
Zarkasih Alfauzi <108111939+arkafauzi@users.noreply.github.com>
2024-01-23 12:35:56 +0700

    Add files via upload

---
+++ db_siswa.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 23 Jan 2024 pada 05.48
+-- Versi server: 10.4.25-MariaDB
+-- Versi PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_siswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `nis` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `jenis_kelamin` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `siswa`
+--
+
+INSERT INTO `siswa` (`nis`, `nama`, `jenis_kelamin`) VALUES
+(221212344, 'Gilang Ramadhan', 'Laki-laki'),
+(992212223, 'Siti', 'Perempuan'),
+(1212212121, 'Zarkasih Alfauzi', 'Laki-Laki');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`nis`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+use function Laravel\Prompts\select;
+
+include 'koneksi.php';
+?>
+
+
+<html>
+    <head>
+    </head>
+        <body>
+            <form action="" method="post">
+                <label>cari : </label>
+                <input type="text" id="username" name="kolomcari" />
+                <button type="submit" id="btncari" name="btncari"> cari</button>
+            </form>
+        <!--table-->
+            <table  border="1" class="data-tabel" cellpadding="0">
+                <thead >
+                    <tr>
+                        <th>NO</th>
+                        <th>Nis</th>
+                        <th>Nama Siswa</th>
+                        <th>Jenis Kelamin</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                         if(isset($_POST['btncari'])){
+                            $cari = $_POST['kolomcari']; 
+                            $data=mysqli_query($db, "SELECT nis, nama, jenis_kelamin from siswa where nis like '%".$cari."%' or 
+                                                nama like '%".$cari."%' or jenis_kelamin like '%".$cari."%'");
+                        }else{
+                            $data=mysqli_query($db, "SELECT nis, nama, jenis_kelamin from siswa ");
+                        }
+                        $no=1;
+                        while($siswa = mysqli_fetch_array($data)){
+                    ?>
+                    <tr>
+                        <td><?php echo $no++; ?></td>
+                        <td><?php echo $siswa['nis'];?></td>
+                        <td><?php echo $siswa['nama'];?></td>
+                        <td><?php echo $siswa['jenis_kelamin'];?></td>
+                    </tr>
+                    <?php
+                    } ?>
+
+                </tbody>
+            </table>
+        <!--penuutup table-->
+        </body>
+</html>
